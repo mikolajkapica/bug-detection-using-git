2f30937983db760c1e607aaa2436d3a5b4a7d5d5
Mikołaj Kapica <mikolajkapica7@gmail.com>
2024-05-19 17:20:47 +0200

    refactor: Refactor models

--- src/models/data_processing.py
+++ src/models/data_processing.py
@@ -1,3 +1,4 @@
+import pickle
 from typing import Callable
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ from sklearn.metrics import (
     ConfusionMatrixDisplay,
     classification_report,
 )
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from src.models.models import Model
 
 
@@ -16,6 +18,20 @@ def load_data(preprocessed_dataset_directory: str) -> pd.DataFrame:
     df = pd.concat([df_normal, df_buggy])
     df = df.fillna(0)
     return df
+
+
+def undersample(df: pd.DataFrame) -> pd.DataFrame:
+    df_normal = df[df["is_bug"] == 0]
+    df_buggy = df[df["is_bug"] == 1]
+    df_normal = df_normal.sample(n=len(df_buggy), random_state=0)
+    return pd.concat([df_normal, df_buggy])
+
+
+def oversample(df: pd.DataFrame) -> pd.DataFrame:
+    df_normal = df[df["is_bug"] == 0]
+    df_buggy = df[df["is_bug"] == 1]
+    df_buggy = df_buggy.sample(n=len(df_normal), replace=True, random_state=0)
+    return pd.concat([df_normal, df_buggy])
 
 
 def split_data(df: pd.DataFrame, test_size: float, random_state: int) -> tuple:
@@ -37,7 +53,7 @@ def predict(model: Model, X_test: pd.DataFrame) -> Series:
 
 
 def plot_confusion_matrix(
-    model: Model, y_test: Series, y_pred: Series, save_location: str
+    model: Model, y_test: Series, y_pred: Series, save_location: str = ""
 ) -> None:
     plt.figure(figsize=(12, 8))
     ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
@@ -58,17 +74,51 @@ def get_classification_report(model: Model, y_test, y_pred) -> str:
     )
 
 
-def pipeline(
+def models_testing(
     preprocessed_dataset_directory: str,
     models: list[Callable[[], Model]],
     test_size: float = 0.2,
     random_state: int = 0,
     save_location: str = "",
+    sampling: str = "none",
 ) -> None:
     df = load_data(preprocessed_dataset_directory)
+    if sampling == "undersample":
+        df = undersample(df)
+    elif sampling == "oversample":
+        df = oversample(df)
     X_train, X_test, y_train, y_test = split_data(df, test_size, random_state)
     for model in models:
         model = train_model(model(), X_train, y_train)
+        pickle.dump(
+            model, open(f"../../trained_models/{model.__class__.__name__}.pkl", "wb")
+        )
         y_pred = predict(model, X_test)
         plot_confusion_matrix(model, y_test, y_pred, save_location)
         print(get_classification_report(model, y_test, y_pred))
+
+
+def plot_decision_tree(preprocessed_dataset_directory: str, save_location: str) -> None:
+    df = load_data(preprocessed_dataset_directory)
+    df = oversample(df)
+    X_train, X_test, y_train, y_test = split_data(df, 0.2, 0)
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    plt.figure(figsize=(40, 30))
+    plot_tree(model, filled=True)
+    plt.title("Decision Tree")
+    plt.savefig(f"{save_location}/decision_tree.png")
+    plt.show()
+
+
+def train_and_save(
+    model: Callable[[], Model], preprocessed_dataset_directory: str, save_location: str
+) -> None:
+    df = load_data(preprocessed_dataset_directory)
+    df = oversample(df)
+    X_train, X_test, y_train, y_test = split_data(df, 0.2, 0)
+    model = train_model(model(), X_train, y_train)
+    y_pred = predict(model, X_test)
+    plot_confusion_matrix(model, y_test, y_pred)
+    print(get_classification_report(model, y_test, y_pred))
+    pickle.dump(model, open(f"{save_location}/{model.__class__.__name__}.pkl", "wb"))
--- src/models/main.py
+++ src/models/main.py
@@ -1,15 +1,22 @@
-from src.models.data_processing import pipeline
+from src.models.data_processing import (
+    models_testing,
+    plot_decision_tree,
+    train_and_save,
+)
 from src.models.models import models
+
 
 PREPROCESSED_DATASET_DIRECTORY = "../../preprocessed_dataset"
 
 
 def main() -> None:
-    pipeline(
+    models_testing(
         PREPROCESSED_DATASET_DIRECTORY,
         models,
-        save_location="../../graphs/results",
+        # save_location="../../graphs/results",
+        sampling="oversample",
     )
+    # train_and_save(models[0], PREPROCESSED_DATASET_DIRECTORY, "../../trained_models")
 
 
 if __name__ == "__main__":
